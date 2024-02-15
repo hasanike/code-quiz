f3475f2ef17403735a7b880ec4c2d5f059855522
hkeidenb <hasani.eidenberger@gmail.com>
2024-02-14 19:13:29 -0800

    CSS framework

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// List of variables the function will run to create the quiz.
+var startBtn = document.querySelector("#start");
+var resetBtn = document.querySelector("#reset")
+var mainEl = document.querySelector(".question")
+var timerEl = document.querySelector("#countdown")
+var answerEl = document.querySelector(".answers")
+var answerResponse = document.querySelector(".answer-Response")
+var scoreEl = document.querySelector (".score")
+
+
